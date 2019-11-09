40d2a3e017827de963bc216686a5417bce16584c
Rod Ellison <rod.r.ellison@gmail.com>
2019-11-09 11:55:16 -0500

    removed bitbucket, added instagram icons and links

--- src/app/components/SiteFooter.jsx
+++ src/app/components/SiteFooter.jsx
@@ -6,12 +6,12 @@ class SiteFooter extends React.Component {
     return (
       <footer>
         <hr/>
-        <p>Website, photos, & content &copy; Rod Ellison 2016</p>
+        <p>Website, photos, & content &copy; Rod Ellison 2019</p>
         <p><a href="mailto:rod.r.ellison@gmail.com">Rod.R.Ellison@gmail.com</a></p>
         <p>
-          <a href="https://bitbucket.com/rodellison" title="Bitbucket"><i className="fa fa-bitbucket fa-lg"/></a>
-          <a href="https://github.com/rodellison" title="Bitbucket"><i className="fa fa-github fa-lg"/></a>
-          <a href="http://www.linkedin.com/in/rodellison" title="LinkedIn"><i className="fa fa-linkedin-square fa-lg"/></a>
+            <a href="http://www.linkedin.com/in/rodellison" title="LinkedIn"><i className="fa fa-linkedin-square fa-lg"/></a>
+            <a href="https://github.com/rodellison" title="Github"><i className="fa fa-github fa-lg"/></a>
+            <a href="https://www.instagram.com/rod.ellison/" title="Instagram"><i className="fa fa-instagram fa-lg"/></a>
         </p>
       </footer>
     );
